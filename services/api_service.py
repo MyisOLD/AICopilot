87d4d9f9223819bc3e1ad11579f1047df9894252
Zyarin <qq2627481035@outlook.com>
2025-01-02 16:50:59 +0800

    更新配置

--- services/api_service.py
+++ services/api_service.py
@@ -1,22 +1,25 @@
+import os
 import httpx
 from openai import OpenAI
 from utils.proxy_utils import ProxyUtils
 
+os.environ["http_proxy"] = "http://localhost:3467"
+os.environ["https_proxy"] = "http://localhost:3467"
 class APIService:
     @staticmethod
     def send_request(conversation_history, user_message, api_url, model_name, api_key, prompt):
         proxy_settings = ProxyUtils.get_win11_proxy_settings()
+
         proxies = {}
         if proxy_settings and proxy_settings["enabled"] and proxy_settings["server"]:
             proxies = {
                 "http://": f"http://{proxy_settings['server']}",
                 "https://": f"https://{proxy_settings['server']}",
             }
-
         client = OpenAI(
             api_key=api_key,
             base_url=api_url,
-            http_client=httpx.Client(proxies=proxies if proxies else None)
+            # http_client=httpx.Client(proxies=proxies if proxies else None)
         )
 
         try:
