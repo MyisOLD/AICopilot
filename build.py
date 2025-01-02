699d572c1347451bc0edd79a5f7b41ecddf0637d
Zyarin <qq2627481035@outlook.com>
2025-01-02 11:21:27 +0800

    更新build脚本

--- build.py
+++ build.py
@@ -17,7 +17,7 @@ def create_exe():
         # '--onefile',  # 打包成单个文件
         '--icon=assets/app_icon.ico',  # 应用图标（如果有）
         f'--add-data=configFiles/config_example.json{os.pathsep}.',  # 添加配置文件
-        f'--add-data=configFiles/prompts_exampe.json{os.pathsep}.',  # 添加提示词模板
+        f'--add-data=configFiles/prompts_example.json{os.pathsep}.',  # 添加提示词模板
         '--clean',  # 清理临时文件
         '--noconfirm',  # 不确认覆盖
     ]
